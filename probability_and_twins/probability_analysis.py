0592ed4285ada4157e431edb708e9f7dbfb59196
Mateus <matbessa12@gmail.com>
2024-07-24 19:49:33 -0300

    occurrence

--- probability_and_twins/probability_analysis.py
+++ probability_and_twins/probability_analysis.py
@@ -19,7 +19,7 @@ def extract_potential_unique_pairs(df):
     unique_p1 = [float(item) for item in list(df['P1'].unique())]
     unique_p2 = [float(item) for item in list(df['P2'].unique())]
     unique_pairs = list(itertools.product(unique_p1, unique_p2))
-    positive_only_pairs = list(filter(lambda x: x[0] > 0 and x[1] > 0, unique_pairs))
+    positive_only_pairs = list(filter(lambda x: x[0] >= 0 and x[1] >= 0, unique_pairs))
     return sorted(positive_only_pairs)
 
 
@@ -45,9 +45,37 @@ def check_for_interruptions(current_row, next_row):
     cross_dt = [datetime.strptime(time, '%H:%M') for time in cross_times]
 
     interruptions = [time.strftime('%H:%M') for time in cross_dt if start_dt < time < end_dt]
-    if len(interruptions) > 0:
-        print(f"Interruptions: {interruptions}")
     return len(interruptions) > 0
+
+
+def check_for_isolation(current_row, next_row) -> bool:
+    """
+    Checks returns if there is any negative value between start_time and end_time.
+    """
+    start_time = current_row.Time1
+    end_time = next_row.Time2
+
+    # Create a dictionary to map times to their respective values
+    time_mapped_to_value = {
+        current_row.Time1: current_row.P1,
+        current_row.Time2: current_row.P2,
+        next_row.Time1: next_row.P1,
+        next_row.Time2: next_row.P2
+    }
+
+    # Convert the start and end times to datetime objects for comparison
+    start_datetime = datetime.strptime(start_time, '%H:%M')
+    end_datetime = datetime.strptime(end_time, '%H:%M')
+
+    # Extract times and values between start_time and end_time
+    times_between = [(time, value) for time, value in time_mapped_to_value.items()
+                     if start_datetime <= datetime.strptime(time, '%H:%M') <= end_datetime]
+
+    # Check for any negative value between start_time and end_time
+    for time, value in times_between:
+        if value < 0:
+            return False
+    return True
 
 
 def generate_pair_occurrence_matrix(merged_df: pd.DataFrame, comparison_range: int = 33):
@@ -62,19 +90,40 @@ def generate_pair_occurrence_matrix(merged_df: pd.DataFrame, comparison_range: i
         for index in range(len(file_df) - 1):
             current_row = file_df.iloc[index]
             new_row = file_df.iloc[index + 1]
+            # Conditional check
+            # filename == "old10-M20-orders-nosl-notp-out.csv" and pair == (202.0, 235.0)
             pair = (current_row.P1, new_row.P2)
             p1, p2 = pair
+            test_pair = pair == (202.0, 235.0)
             if p1 < 0 or p2 < 0:
                 continue
             diff = abs(p2 - p1)
             if diff < comparison_range:
+                if test_pair:
+                    print(f"Test pair failed on diff {filename}")
                 continue
             interruption = check_for_interruptions(current_row, new_row)
             if interruption:
+                if test_pair:
+                    print(f"Test pair failed on interruption {filename}")
                 continue
-            pass
-
-    debug_pair = (235.0, 202.0)
+            isolation = check_for_isolation(current_row, new_row)
+            if not isolation:
+                if test_pair:
+                    print(f"Test pair failed on isolation {filename}")
+                continue
+            pair_dict[pair][filename] += 1
+    # filtered_pair_dict = {
+    #     pair: inner_dict
+    #     for pair, inner_dict in pair_dict.items()
+    #     if any(value != 0 for value in inner_dict.values())
+    # }
+    # sorted_pair_dict = dict(sorted(
+    #     filtered_pair_dict.items(),
+    #     key=lambda item: sum(1 for val in item[1].values() if val == 1),
+    #     reverse=True
+    # ))
+    debug_pair = (202.0, 235.0)
     debug_pair_occurrence = pair_dict[debug_pair]
     # Convert the pair dictionary to a DataFrame
     matrix_df = pd.DataFrame.from_dict(pair_dict, orient='index')
