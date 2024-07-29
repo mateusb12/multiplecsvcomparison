d9552aa4bfc85154445c448ebb4e4b7b2e5f59f2
Mateus <matbessa12@gmail.com>
2024-07-29 09:32:49 -0300

    format adjustments

--- scripts/find_identical_pairs.py
+++ scripts/find_identical_pairs.py
@@ -1,3 +1,5 @@
+import re
+
 import pandas as pd
 
 from core.primitive_pairs import generate_primitive_pairs
@@ -16,16 +18,19 @@ def main():
     for pair in primitive_pairs:
         p1, p2 = pair
         occurrence_dict, row_index_dict = generate_pair_occurrence_dict(merged_csv, (p1, p2), COMPARISON_RANGE)
-        pair_result = {"P1": p1, "P2": p2, "row_index_dict": row_index_dict}
+        pair_result = {"P1": p1, "P2": p2}
         total_occurrences = 0
         for key, value in occurrence_dict.items():
             total_occurrences += value
-        probability = total_occurrences / len(occurrence_dict.keys())
+        probability = total_occurrences / len(occurrence_dict.keys()) * 100
         pair_result["total_occurrences"] = total_occurrences
-        pair_result["probability"] = probability
-        for key_, value_ in occurrence_dict.items():
-            if key_.endswith('.csv'):
-                pair_result[key_] = value_
+        pair_result["probability"] = f"{probability:.2f}%"
+        for occurrence_key, occurrence_value in occurrence_dict.items():
+            match = re.search(r'([a-zA-Z]+\.csv)$', occurrence_key)
+            if match:
+                tag = match.group(1)
+                pair_result[f"{tag} row index"] = row_index_dict[occurrence_key] + 1
+                pair_result[f"{tag} total occurrences"] = occurrence_value
         results.append(pair_result)
     dataframe = pd.DataFrame(results)
     dataframe = move_column(dataframe, 'probability', 3)
